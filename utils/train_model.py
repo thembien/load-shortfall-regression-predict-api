56cf9663470ddc86680e93b07833c34b1d16df4c
thembie <agapenompumelelo@gmail.com>
2023-09-03 15:24:45 +0200

    added my own model

--- utils/train_model.py
+++ utils/train_model.py
@@ -1,31 +1,30 @@
-"""
-    Simple file to create a sklearn model for deployment in our API
 
-    Author: Explore Data Science Academy
-
-    Description: This script is responsible for training a simple linear
-    regression model which is used within the API for initial demonstration
-    purposes.
-
-"""
-
-# Dependencies
 import pandas as pd
 import pickle
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
-# Fetch training data and preprocess for modeling
+
 train = pd.read_csv('./data/df_train.csv')
 
-y_train = train[['load_shortfall_3h']]
-X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+selected_features = [
+    'Seville_humidity',
+    'Valencia_temp',
+    'Barcelona_temp_max',
+]
 
-# Fit model
-lm_regression = LinearRegression(normalize=True)
-print ("Training Model...")
-lm_regression.fit(X_train, y_train)
+X_train = train[selected_features]  # Features
+y_train = train['load_shortfall_3h']
+
+# Instantiate the Random Forest Regressor model
+rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
+
+# Train the model
+rf_model.fit(X_train, y_train)
 
 # Pickle model for use within our API
 save_path = '../assets/trained-models/load_shortfall_simple_lm_regression.pkl'
+
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(lm_regression, open(save_path,'wb'))
+
+with open(save_path, 'wb') as model_file:
+    pickle.dump(rf_model, model_file)
