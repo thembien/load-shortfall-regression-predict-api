465ad84613ccc4a048ec8fb15ebfe42be707103c
thembie <agapenompumelelo@gmail.com>
2023-09-03 15:33:08 +0200

    added my custom model

--- model.py
+++ model.py
@@ -58,8 +58,16 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    # predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
     # ------------------------------------------------------------------------
+
+    selected_features = [
+        'Seville_humidity',
+        'Valencia_temp',
+        'Barcelona_temp_max',
+    ]
+
+    predict_vector = feature_vector_df[selected_features]
 
     return predict_vector
 
